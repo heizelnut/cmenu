5af7adfc078f06f0f94408d4c4046fa7de403290
heizelnut <git@heiz.cc>
2023-01-08 20:33:51 +0100

    Remove useless macros

--- pair.h
+++ pair.h
@@ -7,7 +7,4 @@ typedef struct {
 } pair_t;
 
 #define PAIR_ZERO (pair_t){0,0}
-#define are_pairs_equal(a, b) ((a).x == (b).x && (a).y == (b).y)
-#define is_pair_zero(pair) are_pairs_equal((pair), (PAIR_ZERO))
-
 #endif
